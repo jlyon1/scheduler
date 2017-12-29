5ed414706c8d96428585c5c0e524cab15d566105
jlyon1 <joeylyon1@gmail.com>
2017-12-29 11:41:49 -0500

    Restructure code

---
+++ scheduler/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"scheduler/scheduler"
+)
+
+func test() {
+	fmt.Printf("asdf")
+}
+
+func main() {
+  s := scheduler.New()
+  _ = s
+	for {
+	}
+}
--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -5,12 +5,29 @@ import (
   "reflect"
 )
 
+type Scheduler struct{
+  jobs []Job
+
+}
+
 type Job struct{
   ToRun interface{}
   args []string
   startTime time.Time
   endTime time.Time
   executed bool
+}
+
+//New returns a new scheduler instance for scheduling jobs
+func New() (*Scheduler){
+  return &Scheduler{}
+}
+
+func NewJob(function interface{}, args ...string)(*Job){
+  j := Job{}
+  j.ToRun = function
+  j.args = args
+  return &j
 }
 
 func (job *Job) Invoke() {
